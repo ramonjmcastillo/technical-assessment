9dd29704000a46434ff1a512eb1464bd2e6b1d99
Josh Castillo <ramonjmcastillo@gmail.com>
2022-05-24 22:23:23 +0800

    Some refactors with the code

--- src/data.js
+++ src/data.js
@@ -2,9 +2,9 @@ export const productDetails = {
   productTitle: "Audio-Technica ATH-MSR7",
   productSubTitle: "2017 Best Headphones of the Year Award Winner",
   detailsText:
-    "imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.",
+    "imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu",
   descriptionText:
-    "dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugit.",
+    "dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam",
   productCurrentPrice: 59.99,
   productOriginalPrice: 89.99,
   headPhoneOptions: [{ label: "Black" }, { label: "Brown" }],
